9c37cc801f2c6fe4a8ef9a0436e5df06ac02b95a
Harmeet Matharoo <hmjatt@gmail.com>
2021-12-20 01:07:41 -0700

    rewrite RockPaperScissors
    rewrote the whole game, need to add more functionality

--- script/script.js
+++ script/script.js
@@ -1,68 +1,40 @@
-function computerPlay() {
-  let randomPossibility = ["ROCK", "PAPER", "SCISSORS"];
-  let randomChoice = randomPossibility[Math.floor(Math.random() * randomPossibility.length)];
-  return randomChoice;
-}
-
-function getPlayerSelection(a) {
-  a = prompt("Select one between 'Rock', 'Paper' or 'Scissors'!!!").toUpperCase();  
-  return a;
-}
-
-function getComputerSelection(b) {
-  b = computerPlay();
-  return b;
-}
-
-let computerScore = 0;
 let playerScore = 0;
+let computerScore = 0;
+let playerScorespan = document.getElementById("player-score");
+let computerScore_span = document.getElementById("computer-score");
+let scoreBoard_div = document.getElementById("scoreboard");
+let result_div = document.getElementById("result");
+let rock_div = document.getElementById("rock");
+let paper_div = document.getElementById("paper");
+let scissors_div = document.getElementById("scissors");
+let gameCount_div = document.getElementById("gamecount");
 
-function playSingleRound() {
-
-  let playerSelection = getPlayerSelection();
-  let computerSelection = getComputerSelection();
-
-  console.log(playerSelection);
-  console.log(computerSelection);
-
-  if (playerSelection === computerSelection) {
-      console.log("Select again");
-  } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-      console.log("You Lose!, Paper covered the Rock");
-      computerScore++;
-  } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-      console.log("You Win!!!, Rock broke the Scissors");
-      playerScore++;
-  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-      console.log("You Win!!!, Paper covered Rock");
-      playerScore++;
-  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-      console.log("You Lose!, Scissors cut the Paper");
-      computerScore++;
-  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-      console.log("You Lose!, Rock broke the Scissors");
-      computerScore++;
-  } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-      console.log("You Win!!!, Scissors cut the Paper");
-      playerScore++;
-  } else {
-      console.log("Select one between 'Rock', 'Paper' or 'Scissors'!!!");
-  }
-  console.log(playerScore,computerScore);
+function getComputerChoice() {
+  const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+  let computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  return computerChoice;
 }
 
-function game() {
-  for (let i = 0; i < 5; i++){
-    playSingleRound();
-  }
+function game(playerChoice) {
+  console.log("computer choice = " + getComputerChoice());
+  console.log("player choice = " + playerChoice);
 }
 
-game();
+function main() {
+  rock_div.addEventListener("click", function() {
+    game("rock");
+  });
 
-if (playerScore > computerScore) {
-  console.log("Player Won!!!");
-} else if (computerScore > playerScore) {
-    console.log("Computer Won!!!");
-} else {
-    console.log("Looks like its a tie");
+  paper_div.addEventListener('click', function() {
+    game("paper");
+  });
+
+  scissors_div.addEventListener('click', function() {
+    game("scissors");
+  });
+
 }
+
+main();
+
+
